c00a3798c9c660106eb5377a1a2789b1aa78a061
etyurev <31918751+etyurev@users.noreply.github.com>
2019-04-11 09:02:20 +0200

    Add files via upload

--- P4GUI/src/mainGUI.cpp
+++ P4GUI/src/mainGUI.cpp
@@ -1,17 +1,15 @@
 #include <ros/ros.h>
-
-#include <opencv2/core.hpp>
+nclude <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-
 #include "sensor_msgs/Image.h"
 #include <sensor_msgs/image_encodings.h>
 #include "sensor_msgs/Range.h"
 #include "std_msgs/Empty.h"
 #include "geometry_msgs/Twist.h"
-
+#include "ardrone_autonomy/Navdata.h"
+//#include "pr2_msgs/AccessPoint.h"
 #include <cv_bridge/cv_bridge.h>
-
 #include <string>
 #include <sstream>
 
@@ -24,6 +22,12 @@ cv::Mat imgFront, imgBottom;
 std::string cmdXStr, cmdYStr, cmdZStr, cmdRotStr;
 std::string sonarString;
 std::string takeOffLandStr;
+std::string batteryPercentString;
+float batteryPercentValue;
+std::string altidString;
+float altitude;
+std::string wifiString;
+float wifi;
 
 void callbackTakeOff(const std_msgs::Empty::ConstPtr& msg){
 
@@ -37,13 +41,6 @@ void callbackLand(const std_msgs::Empty::ConstPtr& msg){
       std::ostringstream ss;
       ss << "Grounded";
      takeOffLandStr = ss.str();
-}
-
-void callbackSonarHeight(const sensor_msgs::Range::ConstPtr& msg){
-
-      std::ostringstream ss;
-      ss << "Sonar height: " << msg->range;
-     sonarString = ss.str();
 }
 
 void callbackCMDVel(const geometry_msgs::Twist::ConstPtr& msg){
@@ -63,7 +60,7 @@ void callbackCMDVel(const geometry_msgs::Twist::ConstPtr& msg){
 void callbackImageFront(const sensor_msgs::ImageConstPtr& msg){
 
      cv_bridge::CvImagePtr img;
-     img = cv_bridge::toCvCopy(msg,sensor_msgs::image_encodings::RGB8);
+     img = cv_bridge::toCvCopy(msg,sensor_msgs::image_encodings::BGR8);
      imgFront = img->image;
 
      resize(imgFront, imgFront, cv::Size(), 2, 2);
@@ -77,12 +74,51 @@ void callbackImageBottom(const sensor_msgs::ImageConstPtr& msg){
 
      resize(imgBottom, imgBottom, cv::Size(), 0.5, 0.5);
 
+
 }
+
+void callbackBattery(const ardrone_autonomy::Navdata::ConstPtr& msg){
+
+      std::ostringstream s1,s2;
+      s1  << msg->batteryPercent << "%";
+      batteryPercentValue=msg->batteryPercent;
+     batteryPercentString = s1.str();
+
+    s2<< msg->altd/10;// << "cm";
+     altitude=msg->altd;
+    altidString = s2.str();
+}
+
+void updateBatteryImage()
+{
+  cv::Mat imgBattery, mask;
+  vector<Mat> rgbLayer;
+  imgBattery = imread("/home/yevgen/ardrone_simulator/src/p4GUI/5percent.png",-1);
+  resize(imgBattery, imgBattery, cv::Size(), 0.1, 0.1);
+  split(imgBattery,rgbLayer);
+  if(imgBattery.channels() == 4)
+    {
+        split(imgBattery,rgbLayer);         // seperate channels
+        Mat cs[3] = { rgbLayer[0],rgbLayer[1],rgbLayer[2] };
+        merge(cs,3,imgBattery);  // glue together again
+        mask = rgbLayer[3];       // png's alpha channel used as mask
+    }
+  imgBattery.copyTo(imgFront(cv::Rect(imgFront.cols*0.95, imgFront.rows*0.019, imgBattery.cols, imgBattery.rows)),mask);
+}
+/*
+void callbackWifi()
+{
+  std::ostringstream wifi;
+  wifi << "Wifi strength: " << msg->signal;
+  wifiString = wifi.str();
+}
+*/
 
 int main(int argc, char **argv) {
 
-  imgFront = imread("/home/albert/Pictures/flowers.jpg");
-  imgBottom = imread("/home/albert/Pictures/flowers.jpg");
+  imgFront = imread("/home/yevgen/Desktop/ros.png");
+  resize(imgFront, imgFront, cv::Size(), 10, 10);
+  imgBottom = imread("/home/yevgen/Desktop/ros.png");
   resize(imgBottom, imgBottom, cv::Size(), 0.1, 0.1);
   ros::init(argc, argv,"Cool");
 
@@ -101,26 +137,38 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh;
   ros::Subscriber sub_ImageFront = nh.subscribe("/ardrone/front/image_raw", 5, callbackImageFront);
   ros::Subscriber sub_ImageBottom = nh.subscribe("/ardrone/bottom/image_raw", 5, callbackImageBottom);
-  ros::Subscriber sub_SonarHeight = nh.subscribe("/sonar_height", 5, callbackSonarHeight);
+  //ros::Subscriber sub_SonarHeight = nh.subscribe("/sonar_height", 5, callbackSonarHeight);
   ros::Subscriber sub_CMDVel = nh.subscribe("/cmd_vel", 5, callbackCMDVel);
   ros::Subscriber sub_TakeOff = nh.subscribe("/ardrone/takeoff", 5, callbackTakeOff);
   ros::Subscriber sub_Land = nh.subscribe("/ardrone/land", 5, callbackLand);
+  ros::Subscriber sub_Bat = nh.subscribe("/ardrone/navdata", 5, callbackBattery);
+  //ros::Subscriber sub_wifi = nh.subscribe("",5,callbackWifi);
+
+
 
   ros::Rate loop_rate(30);
 
     while (ros::ok()){
+      float alt=0.7-altitude*0.00025;
+      float batOrig=0.95+0.00033*batteryPercentValue;
 
       cv::putText(imgFront, sonarString, Point2f(imgFront.cols*0.05, imgFront.rows*0.95), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
-      cv::putText(imgFront, cmdZStr, Point2f(imgFront.cols*0.05, imgFront.rows*0.92), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
-      cv::putText(imgFront, cmdXStr, Point2f(imgFront.cols*0.05, imgFront.rows*0.89), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
-      cv::putText(imgFront, cmdRotStr, Point2f(imgFront.cols*0.05, imgFront.rows*0.86), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
-      cv::putText(imgFront, takeOffLandStr, Point2f(imgFront.cols*0.05, imgFront.rows*0.83), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
-      //cv::putText(imgMat, cmdYStr, Point2f(imgMat.cols*0.05, imgMat.rows*0.85), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
+if (takeOffLandStr=="Grounded")
+{
+  cv::putText(imgFront, "GROUNDED", Point2f(imgFront.cols*0.01, imgFront.rows*0.78), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
+}
+else
+{
+  cv::putText(imgFront, "FLYING", Point2f(imgFront.cols*0.01, imgFront.rows*0.13), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
+}
+cv::putText(imgFront, batteryPercentString, Point2f(imgFront.cols*0.91, imgFront.rows*0.047), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
 
+      updateBatteryImage();
+      rectangle(imgFront, Point(imgFront.cols*0.01, imgFront.rows*0.20), Point(imgFront.cols*0.06, imgFront.rows*0.70),(0,0,255),-1); //Altitude scala
+      rectangle(imgFront, Point(imgFront.cols*0.02,imgFront.rows*alt), Point(imgFront.cols*0.05, imgFront.rows*0.70),(0,255,0),-1);
+      cv::putText(imgFront, altidString, Point2f(imgFront.cols*0.03, imgFront.rows*(alt-0.02)), FONT_HERSHEY_DUPLEX, 0.7, Scalar(255, 255, 255), 2, 8, false);
+      rectangle(imgFront, Point(imgFront.cols*0.955,imgFront.rows*0.025), Point(imgFront.cols*batOrig, imgFront.rows*0.058),(0,0,255),-1);
 
-
-      imgBottom.copyTo(imgFront(cv::Rect(imgFront.cols*0.72, imgFront.rows*0.72, imgBottom.cols, imgBottom.rows)));
-      rectangle(imgFront,Point((imgFront.cols*0.72)-1,(imgFront.rows*0.72)-1),Point((imgFront.cols*0.72+imgBottom.cols)+1,(imgFront.rows*0.72+imgBottom.rows)+1),(0,255,0),2);
       cv::imshow("Ar Drone",imgFront);
       waitKey(1);
 
